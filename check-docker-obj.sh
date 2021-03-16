17c53b32c9939e684650fd90a61a16d1772e804b
martinovich89 <dsinemoi1mouton@gmail.com>
2021-03-16 19:32:09 +0100

    raze dat shit

--- check-docker-obj.sh
+++
@@ -1,4 +0,0 @@
-docker network ls | grep "my_network"
-docker inspect my_network
-#docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
-docker ps -a && docker images -a
--- clean_docker.sh
+++
@@ -1,3 +0,0 @@
-docker stop $(docker ps -qa)
-docker container rm $(docker ps -qa)
-docker rmi -f $(docker images -qa)
